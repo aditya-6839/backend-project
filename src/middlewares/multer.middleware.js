c5ffaa08152d4b26d9310856abb350b36d2713f9
Aditya Fulsoundar <adityafulsoundar41@gmail.com>
2025-08-10 13:30:18 +0530

    add multer cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+// Handle file uploads & Save files to disk
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({ storage, })
